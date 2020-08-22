6dddd5dd1701a95e3c116d8bea99c2bafca5bc24
vipdostyp <68096381+vipdostyp@users.noreply.github.com>
2020-08-22 13:27:15 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+new WOW().init();
+var mySwiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".project-pagination",
+    bulletClass: "projects-bullet",
+    bulletActiveClass: "projects-bullet-active",
+    clickable: true,
+  },
+});
